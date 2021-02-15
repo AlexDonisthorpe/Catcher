df3cbac2f94157bedce5096c3b117645704dd937
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-02-15 02:26:17 +0000

    Added a new shield resource prefab with one of the new assets, & attached the newly renamed object mover script to it

--- Assets/Scripts/ObjectMovement.cs
+++ Assets/Scripts/ObjectMovement.cs
@@ -3,17 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleMovement : MonoBehaviour
+public class ObjectMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 4f;
-    private float _step;
     
     private Vector2 _destination;
-
-    private void Awake()
-    {
-        _step = Time.deltaTime * speed;
-    }
 
     void Start()
     {
@@ -25,13 +19,9 @@ public class ObstacleMovement : MonoBehaviour
         _destination = new Vector2(transform.position.x, Camera.main.ScreenToWorldPoint(Vector2.zero).y);
     }
 
-    private void Update()
-    {
-        _step = Time.deltaTime * speed;
-    }
-
     void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, _destination, _step);
+        float step = Time.deltaTime * speed;
+        transform.position = Vector2.MoveTowards(transform.position, _destination, step);
     }
 }
