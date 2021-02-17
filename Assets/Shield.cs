33ce18fba74a8b232b39a85da9632681df223f6b
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-02-17 02:58:42 +0000

    Added the shield script which handles the shield resources. The Shield script currently tracks the collision with the resource and updates the shield hits available, shield hits are also removed on inpact with an obstacle

---
+++ Assets/Shield.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [SerializeField] float hitsUntilDeath = 4f;
+    [SerializeField] private float energyPerShield = 1f;
+    
+    [Header("Testing")]
+    [SerializeField] float currentEnergy = 0;
+    [SerializeField] float currentHitsRemaining = 0;
+
+    private void Awake()
+    {
+        currentHitsRemaining = hitsUntilDeath;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            currentHitsRemaining--;
+        } else if (other.CompareTag("Energy"))
+        {
+            UpdateEnergy();
+        }
+    }
+
+    private void UpdateEnergy()
+    {
+        currentEnergy++;
+        if (currentEnergy % energyPerShield == 0)
+        {
+            currentHitsRemaining = Mathf.Clamp(currentHitsRemaining++,0, hitsUntilDeath);
+        }
+    }
+}
